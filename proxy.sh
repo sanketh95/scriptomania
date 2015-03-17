1617be728b0cb71f88a2ebe75d5b87ba4191f72f
Sanketh Mopuru <sanketh.mopuru@gmail.com>
2015-03-17 17:53:51 +0530

    added settings for other protocols

--- proxy.sh
+++ proxy.sh
@@ -9,15 +9,22 @@ then
 fi
 
 `gsettings set org.gnome.system.proxy mode 'manual'`
-`gsettings set org.gnome.system.proxy.http enabled true`
 `gsettings set org.gnome.system.proxy.http host $1`
+`gsettings set org.gnome.system.proxy.https host $1`
+`gsettings set org.gnome.system.proxy.ftp host $1`
+`gsettings set org.gnome.system.proxy.socks host $1`
 if [ "$#" -gt 1 ]
 then
 	`gsettings set org.gnome.system.proxy.http port $2`
+	`gsettings set org.gnome.system.proxy.https port $2`
+	`gsettings set org.gnome.system.proxy.ftp port $2`
+	`gsettings set org.gnome.system.proxy.socks port $2`
 else
 	`gsettings set org.gnome.system.proxy.http port $DEFAULT_PORT`
+	`gsettings set org.gnome.system.proxy.https port $DEFAULT_PORT`
+	`gsettings set org.gnome.system.proxy.ftp port $DEFAULT_PORT`
+	`gsettings set org.gnome.system.proxy.socks port $DEFAULT_PORT`
 fi
-`gsettings set org.gnome.system.proxy use-same-proxy true`
 
 if [ "$#" = 4 ]
 then
